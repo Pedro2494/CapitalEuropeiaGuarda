f9b974301aa2979cc1dce16b97b4e3598de29a72
joaomftorres <joaomftorres@gmail.com>
2020-12-18 11:05:16 +0000

    Criação do modelo Local

---
+++ CapitalEuropeiaGuarda/Models/Local.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapitalEuropeiaGuarda.Models
+{
+    public class Local
+    {
+        public int localID { get; set; }
+
+        [Required(ErrorMessage = "Insira o nome do Local")]
+        public string Nome { get; set; }
+
+        public string Concelho { get; set; }
+
+        public string Coordenadas { get; set; }
+    }
+}
