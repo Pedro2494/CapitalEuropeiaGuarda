73df021b3ca1f4a05ea0ce8725e708e8fd980b79
Pedro _ <antunes-pedro@hotmail.com>
2020-12-27 17:22:28 +0000

    Correção de erros

--- CapitalEuropeiaGuarda/Controllers/HomeController.cs
+++ CapitalEuropeiaGuarda/Controllers/HomeController.cs
@@ -13,17 +13,14 @@ namespace CapitalEuropeiaGuarda.Controllers
 {
     public class HomeController : Controller
     {
-        SqlCommand com = new SqlCommand();
-        SqlDataReader dr;
-        SqlConnection con = new SqlConnection();
-        List<PontoInteresse> pontosinteresse = new List<PontoInteresse>();
+        
 
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            con.ConnectionString = CapitalEuropeiaGuarda.Properties.Resources.ConnectionString;
+            
         }
 
         public IActionResult Index()
@@ -45,40 +42,6 @@ namespace CapitalEuropeiaGuarda.Controllers
             return View();
         }
 
-        public IActionResult ViewPontos()
-        {
-            FetchData();
-            return View(pontosinteresse);
-        }
-
-        private void FetchData()
-        {
-            if(pontosinteresse.Count > 0)
-            {
-                pontosinteresse.Clear();
-            }
-
-            try
-            {
-                con.Open();
-                com.Connection = con;
-                com.CommandText = "SELECT TOP (1000) [PontoInteresseId],[Nome],[Local],[DescricaoCurta] FROM [PontoInteresse]";
-                dr = com.ExecuteReader();
-                while (dr.Read())
-                {
-                    pontosinteresse.Add(new PontoInteresse() { 
-                        Nome = dr ["Nome"].ToString()
-                        ,Local = dr ["Local"].ToString()
-                        ,DescricaoCurta = dr["DescricaoCurta"].ToString()
-                    });
-                }
-                con.Close();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
 
         public IActionResult Register()
         {
